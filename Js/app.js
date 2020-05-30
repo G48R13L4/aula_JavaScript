c5d2be6860fa1296db23efe4b6f3bcb3ae36e724
Gabriela <gabriela_fs11@hotmail.com>
2020-05-30 13:43:32 -0300

    aula1_js

---
+++ Js/app.js
@@ -0,0 +1,10 @@
+var nome = 'Gabriela Ferreira';
+var idade = 18;
+var ano = 2002;
+var frase = 'china é o melhor';
+
+console.log(frase.replace('china', 'brasil'));
+
+
+console.log(idade + ano);
+console.log('Bem-vindo(a) ' + nome + ' sua idade é ' + idade + ' anos');
